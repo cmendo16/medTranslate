643ed65448de84c701d3ad01290a3eb467759660
Cristina Mendoza <cmendosalazn22@gmail.com>
2025-05-19 11:36:23 -0400

    create views for each model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from .models import Category, Favorite, Phrase, Profile
+from .serializers import CategorySerializer, FavoriteSerializer, PhraseSerializer, ProfileSerializer
+
 
 # Create your views here.
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet): 
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+class FavoriteViewSet(viewsets.ModelViewSet):
+    serializer_class = FavoriteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Favorite.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+class PhraseViewSet(viewsets.ModelViewSet): 
+    queryset = Phrase.objects.all()
+    serializer_class = PhraseSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+class ProfileViewSet(viewsets.ReadOnlyModelViewSet): 
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
