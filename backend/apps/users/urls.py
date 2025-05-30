f80ae8705c314a18164e3683751aa3e4b729f746
Cristina Mendoza <cmendosalazn22@gmail.com>
2025-05-29 20:16:58 -0400

    update url paths

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProfileViewSet
+from .views import ProfileViewSet, SignupView
 
 router = DefaultRouter()
 router.register(r'profiles', ProfileViewSet, basename='profile')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('signup/', SignupView.as_view(), name='signup'),
+
 ]
--- backend/medtranslate_backend/urls.py
+++ backend/medtranslate_backend/urls.py
@@ -24,7 +24,10 @@ from apps.users.views import SignupView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/auth/signup/',          SignupView.as_view(),        name='signup'),
+    path('api/auth/', include('apps.users.urls')),
+    path('api/auth/', include('apps.favorites.urls')),
+    path('api/auth/', include('apps.categories.urls')),
+    path('api/auth/', include('apps.phrases.urls')),
     path('api/auth/token/',           TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/',   TokenRefreshView.as_view(),    name='token_refresh'),
 
