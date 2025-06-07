390ad22dad3a030a78d01f986f49623bdefc10f0
Cristina Mendoza <cmendosalazn22@gmail.com>
2025-06-07 13:37:56 -0400

    update user profile view API

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,18 +1,20 @@
 from rest_framework import permissions, viewsets, generics, throttling
-from .models import  Profile
+from .models import Profile
 from .serializers import ProfileSerializer, SignupSerializer
 from django.contrib.auth import get_user_model
 
 # Create your views here.
 User = get_user_model()   
-class ProfileViewSet(viewsets.ReadOnlyModelViewSet): 
-    queryset = Profile.objects.all()
+class MyProfileView(generics.RetrieveUpdateDestroyAPIView): 
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+    def get_object(self): 
+        return self.request.user.profile # type: ignore
     
 class SignupView(generics.CreateAPIView): 
     serializer_class = SignupSerializer
     # allow any user to sign up 
     permission_classes = [permissions.AllowAny]
     throttle_classes   = [throttling.UserRateThrottle]
-    
+     
