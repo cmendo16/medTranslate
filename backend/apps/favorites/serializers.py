8347a798880d417c2f6d1103fd1f98c2b1b3949a
Cristina Mendoza <cmendosalazn22@gmail.com>
2025-06-12 18:11:25 -0400

    update favorites serializers file to include Phrase information in API response

--- backend/apps/favorites/serializers.py
+++ backend/apps/favorites/serializers.py
@@ -1,7 +1,20 @@
 from .models import Favorite
 from rest_framework import serializers
+from ..phrases.models import Phrase
 
+class FavoritePhraseSerializer(serializers.ModelSerializer): 
+    class Meta: 
+        model = Phrase
+        fields = ("text_original", "text_translation")
 class FavoriteSerializer(serializers.ModelSerializer):
+    
+    favorite_phrase = FavoritePhraseSerializer(read_only=True, source ="phrase")
+    
+    phrase_id = serializers.PrimaryKeyRelatedField(
+        queryset=Phrase.objects.all(),
+        write_only=True,
+         source="phrase",
+    )
     class Meta:
         model  = Favorite
-        fields = ('id','user','phrase','created_at')
+        fields = '__all__' 
