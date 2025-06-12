3e3ae8e686b55dec39b5971ee97f160c52c7dd72
Cristina Mendoza <cmendosalazn22@gmail.com>
2025-06-12 18:12:37 -0400

    update phrases viewset

--- backend/apps/phrases/views.py
+++ backend/apps/phrases/views.py
@@ -5,7 +5,7 @@ from .serializers import PhraseSerializer
 from rest_framework import permissions, viewsets
 
 # Create your views here.
-class PhraseViewSet(viewsets.ModelViewSet): 
+class PhraseViewSet(viewsets.ReadOnlyModelViewSet): 
     queryset = Phrase.objects.all()
     serializer_class = PhraseSerializer
     permission_classes = [permissions.IsAuthenticated]
