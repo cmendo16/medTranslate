046ee74a7efdc8c0d0356af5725173a638ed76b7
Cristina Mendoza <cmendosalazn22@gmail.com>
2025-07-01 14:12:57 -0400

    Update App.jsx and index.js for basic structure of app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'; 
 import './App.css';
+import LoginPage from '../src/pages/Login';
+import Home from './pages/Home';
 function App() { 
   return (
     <>
-    <div>This will be the home page for the app.</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path = '/' element={<Home />} />
+      <Route path ='/login' element={<LoginPage />}/>
+
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
